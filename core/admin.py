8cd1d7c146899ac85b882380e0f19245bed5ae97
Yash Gupta <yg291557@gmail.com>
2026-06-24 17:04:42 +0530

    update adm

--- core/admin.py
+++ core/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-# Register your model
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    # 1. This adds the handle as a column on the main list of all users
+    list_display = ('username', 'email', 'codeforces_handle', 'is_staff')
+    
+    # 2. This adds a new section on the actual edit page so you can change it
+    fieldsets = UserAdmin.fieldsets + (
+        ('Algolytics Profile Data', {
+            'fields': ('codeforces_handle',),
+        }),
+    )
+
+# Register the model with these new custom admin settings
+admin.site.register(CustomUser, CustomUserAdmin)
